28855cea569b794807ff9e2f2518f60b15daed9f
“achuyko” <achuyko@algoritmiclab.ai>
2024-08-15 14:46:57 +0300

    Add gRPC server and client for CRUD operations on chat API

---
+++ cmd/grpc_server/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/brianvoe/gofakeit"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	"github.com/Chuiko-GIT/chat-server/pkg/chat_api"
+)
+
+const (
+	grpcPort = 50051
+)
+
+type server struct {
+	chat_api.UnimplementedChatApiServer
+}
+
+func (s server) Create(ctx context.Context, req *chat_api.CreateRequest) (*chat_api.CreateResponse, error) {
+	return &chat_api.CreateResponse{Id: gofakeit.Int64()}, nil
+}
+
+func (s server) Delete(ctx context.Context, req *chat_api.DeleteRequest) (*emptypb.Empty, error) {
+	return &emptypb.Empty{}, nil
+}
+
+func (s server) SendMessage(ctx context.Context, req *chat_api.SendMessageRequest) (*emptypb.Empty, error) {
+	return &emptypb.Empty{}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	chat_api.RegisterChatApiServer(s, server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
